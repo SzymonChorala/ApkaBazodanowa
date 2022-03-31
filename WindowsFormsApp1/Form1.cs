bd572bfc303961ff99edea3e7e8dff991414ec86
Kuba Czerniak <kubaczarny3@gmail.com>
2022-03-31 17:42:54 +0200

    Added saving values to and getting values from database.

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -14,8 +14,6 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        Waluta waluta = new Waluta();
-        string call = "https://openexchangerates.org/api/latest.json?app_id=a88a77c6b5bb401ca088160709305677";
         public Form1()
         {
             InitializeComponent();
@@ -32,73 +30,82 @@ namespace WindowsFormsApp1
 
         private async void button_Click(object sender, EventArgs e)
         {
+            Waluta waluta = new Waluta();
+            string date = dateTimePicker1.Text;
+            string[] daty = date.Split('/');
+            string call = "https://openexchangerates.org/api/historical/" + daty[2] + "-" + daty[0] + "-" + daty[1] + ".json?app_id=a88a77c6b5bb401ca088160709305677";
             HttpClient client = new HttpClient();
             string response = await client.GetStringAsync(call);
             waluta = JsonConvert.DeserializeObject<Waluta>(response);
             var context = new Przelicznik();
-            context.Waluty.Add(new WalutaBaz
+
+            var val = new WalutaBaz();
+            if (context.Waluty.Any(x => x.Date == date))
+                val = context.Waluty.First(x => x.Date == date);
+            else
             {
-                disclaimer = waluta.disclaimer,
-                license = waluta.license,
-                timestamp = waluta.timestamp,
-                Base = waluta.Base,
-                ratesPLN = waluta.rates["PLN"],
-                ratesCHF = waluta.rates["CHF"],
-                ratesEUR = waluta.rates["EUR"],
-                ratesGBP = waluta.rates["GBP"],
-                ratesUSD = waluta.rates["USD"],
-                Date = dateTimePicker1.Text
+                val=context.Waluty.Add(new WalutaBaz
+                {
+                    disclaimer = waluta.disclaimer,
+                    license = waluta.license,
+                    timestamp = waluta.timestamp,
+                    Base = waluta.Base,
+                    ratesPLN = waluta.rates["PLN"],
+                    ratesCHF = waluta.rates["CHF"],
+                    ratesEUR = waluta.rates["EUR"],
+                    ratesGBP = waluta.rates["GBP"],
+                    ratesUSD = waluta.rates["USD"],
+                    Date = date
+                }
+                );
+                context.SaveChanges();
             }
-            ) ;
-            context.SaveChanges();
             
             if (textBoxPLN.Text != "")
             {
                 double zloty = Convert.ToDouble(textBoxPLN.Text);
-                textBoxEUR.Text = ((zloty * waluta.rates["EUR"])/waluta.rates["PLN"]).ToString("#.##");
-                textBoxGBP.Text = ((zloty * waluta.rates["GBP"]) / waluta.rates["PLN"]).ToString("#.##");
-                textBoxCHF.Text = ((zloty * waluta.rates["CHF"]) / waluta.rates["PLN"]).ToString("#.##");
-                textBoxUSD.Text = (zloty / waluta.rates["PLN"]).ToString("#.##");
+
+                textBoxEUR.Text = ((zloty * val.ratesEUR)/val.ratesPLN).ToString("#.##");
+                textBoxGBP.Text = ((zloty * val.ratesGBP) / val.ratesPLN).ToString("#.##");
+                textBoxCHF.Text = ((zloty * val.ratesCHF) / val.ratesPLN).ToString("#.##");
+                textBoxUSD.Text = (zloty / val.ratesPLN).ToString("#.##");
             }
             if (textBoxEUR.Text != "")
             {
                 double euro = Convert.ToDouble(textBoxEUR.Text);
-                textBoxPLN.Text = ((euro * waluta.rates["PLN"]) / waluta.rates["EUR"]).ToString("#.##");
-                textBoxGBP.Text = ((euro * waluta.rates["GBP"]) / waluta.rates["EUR"]).ToString("#.##");
-                textBoxCHF.Text = ((euro * waluta.rates["CHF"]) / waluta.rates["EUR"]).ToString("#.##");
-                textBoxUSD.Text = (euro / waluta.rates["EUR"]).ToString("#.##");
+                textBoxPLN.Text = ((euro * val.ratesPLN) / val.ratesEUR).ToString("#.##");
+                textBoxGBP.Text = ((euro * val.ratesGBP) / val.ratesEUR).ToString("#.##");
+                textBoxCHF.Text = ((euro * val.ratesCHF) / val.ratesEUR).ToString("#.##");
+                textBoxUSD.Text = (euro / val.ratesEUR).ToString("#.##");
             }
             if (textBoxGBP.Text != "")
             {
                 double funt = Convert.ToDouble(textBoxGBP.Text);
-                textBoxPLN.Text = ((funt * waluta.rates["PLN"]) / waluta.rates["GBP"]).ToString("#.##");
-                textBoxEUR.Text = ((funt * waluta.rates["EUR"]) / waluta.rates["GBP"]).ToString("#.##");
-                textBoxCHF.Text = ((funt * waluta.rates["CHF"]) / waluta.rates["GBP"]).ToString("#.##");
-                textBoxUSD.Text = (funt / waluta.rates["GBP"]).ToString("#.##");
+                textBoxPLN.Text = ((funt * val.ratesPLN) / val.ratesGBP).ToString("#.##");
+                textBoxEUR.Text = ((funt * val.ratesEUR) / val.ratesGBP).ToString("#.##");
+                textBoxCHF.Text = ((funt * val.ratesCHF) / val.ratesGBP).ToString("#.##");
+                textBoxUSD.Text = (funt / val.ratesGBP).ToString("#.##");
             }
             if (textBoxCHF.Text != "")
             {
                 double frank = Convert.ToDouble(textBoxCHF.Text);
-                textBoxPLN.Text = ((frank * waluta.rates["PLN"]) / waluta.rates["CHF"]).ToString("#.##");
-                textBoxEUR.Text = ((frank * waluta.rates["EUR"]) / waluta.rates["CHF"]).ToString("#.##");
-                textBoxGBP.Text = ((frank * waluta.rates["GBP"]) / waluta.rates["CHF"]).ToString("#.##");
-                textBoxUSD.Text = (frank / waluta.rates["CHF"]).ToString("#.##");
+                textBoxPLN.Text = ((frank * val.ratesPLN) / val.ratesCHF).ToString("#.##");
+                textBoxEUR.Text = ((frank * val.ratesEUR) / val.ratesCHF).ToString("#.##");
+                textBoxGBP.Text = ((frank * val.ratesGBP) / val.ratesCHF).ToString("#.##");
+                textBoxUSD.Text = (frank / val.ratesCHF).ToString("#.##");
             }
             if (textBoxUSD.Text != "")
             {
                 double dolar = Convert.ToDouble(textBoxUSD.Text);
-                textBoxPLN.Text = ((dolar * waluta.rates["PLN"]) / waluta.rates["USD"]).ToString("#.##");
-                textBoxEUR.Text = ((dolar * waluta.rates["EUR"]) / waluta.rates["USD"]).ToString("#.##");
-                textBoxGBP.Text = ((dolar * waluta.rates["GBP"]) / waluta.rates["USD"]).ToString("#.##");
-                textBoxCHF.Text = (dolar* waluta.rates["CHF"]).ToString("#.##");
+                textBoxPLN.Text = ((dolar * val.ratesPLN) / val.ratesUSD).ToString("#.##");
+                textBoxEUR.Text = ((dolar * val.ratesEUR) / val.ratesUSD).ToString("#.##");
+                textBoxGBP.Text = ((dolar * val.ratesGBP) / val.ratesUSD).ToString("#.##");
+                textBoxCHF.Text = (dolar* val.ratesCHF).ToString("#.##");
             }
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-            string data = dateTimePicker1.Text;
-            string[] daty =data.Split('/');
-            call = "https://openexchangerates.org/api/historical/" + daty[2] + "-" + daty[0] + "-" + daty[1] + ".json?app_id=a88a77c6b5bb401ca088160709305677";
                     
         }
 
